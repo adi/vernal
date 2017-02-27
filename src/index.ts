ea402ce05723bdd945434b5fa9e64e39c4356e83
Adrian Punga <adrian.punga@everymatrix.com>
2017-02-27 20:22:32 +0200

    Fixed object instantiation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-export interface VernalPlugin {
-  run(): Promise<any>;
-}
-
-export enum ComponentType {
+export enum BeanType {
   SINGLETON,
   PROTOTYPE,
+  VALUE,
 }
 
 export class VernalError extends Error {
@@ -13,183 +10,182 @@ export class VernalError extends Error {
   }
 }
 
-export class VernalRunError extends VernalError {
+interface Bean {
+  instance: Object;
 }
 
-export class VernalInitError extends VernalError {
+interface BeanMap {
+  [key: string]: Bean
 }
 
-export class VernalGetComponentError extends VernalError {
+interface BeanDescriptor {
+  beanClass: { new (): any };
+  beanType: BeanType;
 }
 
-export class VernalInvalidComponentTypeError extends VernalError {
+interface BeanDescriptorMap {
+  [key: string]: BeanDescriptor
 }
 
-export class VernalInjectComponentError extends VernalError {
+interface Injection {
+  propertyKey: String;
+  propertyBeanName: String;
 }
 
-export class VernalPossibleDependencyCycleError extends VernalError {
-}
-
-export class VernalPluginError extends VernalError {
-}
+const INJECTIONS_PROPERTY = '__injections';
+const POST_CONSTRUCT_HOOKS_PROPERTY = '__post_construct_hooks';
 
 class VernalApplication {
 
-  private components = {};
-  private autowires = [];
-  private plugins: VernalPlugin[] = [];
+  private beans: BeanMap = {};
+  private beanDescriptors: BeanDescriptorMap = {};
 
-  registerPlugin(plugin: VernalPlugin) {
-    this.plugins.push(plugin);
+  registerBean(beanClass: { new (): any }, beanType: BeanType) {
+    this.beanDescriptors[beanClass.name] = <BeanDescriptor>{ beanClass, beanType };
+    // console.log('registerBean', beanClass.name, beanType);
+  }
+  registerInstance(beanInstance: Object) {
+    const beanClass = beanInstance.constructor;
+    const beanName = beanClass.name;
+    this.beans[beanName] = <Bean>{ instance: beanInstance };
+    this.beanDescriptors[beanName] = <BeanDescriptor>{ beanClass, beanType: BeanType.VALUE };
+    // console.log('registerInstance', beanName);
+  }
+  registerValue(beanName: string, beanValue: any) {
+    this.beans[beanName] = <Bean>{ instance: beanValue };
+    this.beanDescriptors[beanName] = <BeanDescriptor>{ beanType: BeanType.VALUE };
+    // console.log('registerValue', beanName);
   }
 
-  hasComponent(componentName: string) {
-    return {}.hasOwnProperty.call(this.components, componentName);
-  }
-
-  async callInitIfAvailable(instance) {
-    try {
-      if (instance.prototype !== undefined) {
-        if (typeof instance.prototype.init === 'function') {
-          return instance.prototype.init.bind(instance)();
-        }
-      } else {
-        if (typeof instance.init === 'function') {
-          return instance.init();
-        }
-      }
-    } catch (e) {
-      throw new VernalInitError(`Error calling while calling ${instance.constructor.name}.init`, e);
+  registerInjection(propertyBeanNameOrClass: string | { new (): any }, beanInstance: Object, propertyKey: String, descriptor: PropertyDescriptor) {
+    if (propertyBeanNameOrClass === undefined) {
+      throw new VernalError(`Component class for component to be injected in '${beanInstance.constructor.name}.${propertyKey}' not loaded yet (perhaps you have a cycle in your dependencies)`);
     }
-  }
-
-  async getComponent(componentName: string) {
-    try {
-      const component = this.components[componentName];
-      if (component === undefined) {
-        throw new VernalGetComponentError(`Component '${componentName}' not found`);
-      }
-      switch (component.type) {
-        case ComponentType.SINGLETON:
-          return component.data;
-        case ComponentType.PROTOTYPE:
-          const componentClass = component.data;
-          const newInstance = new componentClass();
-          // Inject dependencies
-          for (const {
-            targetName,
-            propertyKey,
-            componentName,
-          } of this.autowires.filter(item => item.targetName === componentName)) {
-            if (!this.hasComponent(componentName)) {
-              throw new VernalInjectComponentError(`Could not find component '${componentName}' in context while trying to inject into '${targetName}.${propertyKey}'`);
-            }
-            const component = await this.getComponent(componentName);
-            Object.defineProperty(newInstance, propertyKey, {
-              value: component,
-              writable: false,
-              configurable: false,
-            });
-          }
-          // Run init (if defined)
-          await this.callInitIfAvailable(newInstance);
-          return newInstance;
-        default:
-          throw new VernalInvalidComponentTypeError(`Component '${componentName}' has invalid type '${component.type}'`);
-      }
-    } catch (e) {
-      throw new VernalGetComponentError(`Error getting component ${componentName}`, e);
+    let propertyBeanName: string;
+    if (typeof propertyBeanNameOrClass === 'string') {
+      propertyBeanName = propertyBeanNameOrClass;
+    } else {
+      propertyBeanName = propertyBeanNameOrClass.name;
     }
-  }
-
-  registerComponent(component: any, type: ComponentType) {
-    this.components[component.name] = { type, data: component };
-  }
-
-  registerComponentInstance(instance: any) {
-    this.components[instance.constructor.name] = { type: ComponentType.SINGLETON, data: instance };
-  }
-
-  registerValue(name: string, value: any) {
-    this.components[name] = { type: ComponentType.SINGLETON, data: value };
-  }
-
-  linkComponent(component: any) {
-    return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
-      let componentName: string;
-      if (typeof component === 'string') {
-        componentName = component;
-      } else {
-        if (component === undefined) {
-          throw new VernalPossibleDependencyCycleError(`Component class for component to be autowired in '${target.constructor.name}.${propertyKey}' not loaded yet (perhaps you have a cycle in your dependencies)`);
-        }
-        componentName = component.name;
-      }
-      this.autowires.push({
-        targetName: target.constructor.name,
-        propertyKey,
-        componentName,
+    if ({}.hasOwnProperty.call(beanInstance, INJECTIONS_PROPERTY) === false) {
+      Object.defineProperty(beanInstance, INJECTIONS_PROPERTY, <TypedPropertyDescriptor<Injection[]>>{
+        configurable: false,
+        writable: false,
+        enumerable: false,
+        value: [],
       });
-      descriptor = { ...descriptor, configurable: true };
-      return descriptor;
-    };
+    }
+    // console.log('registerInjection', beanInstance.constructor.name, propertyKey, propertyBeanName);
+    beanInstance[INJECTIONS_PROPERTY].push(<Injection>{ propertyKey, propertyBeanName });
+    descriptor = { ...descriptor, configurable: true };
+    return descriptor;
+  }
+  registerPostConstructHook(beanInstance: Object, hookName: String) {
+    if ({}.hasOwnProperty.call(beanInstance, POST_CONSTRUCT_HOOKS_PROPERTY) === false) {
+      Object.defineProperty(beanInstance, POST_CONSTRUCT_HOOKS_PROPERTY, <TypedPropertyDescriptor<String[]>>{
+        configurable: false,
+        writable: false,
+        enumerable: false,
+        value: [],
+      });
+    }
+    // console.log('registerPostConstructHook', beanInstance.constructor.name, hookName);
+    beanInstance[POST_CONSTRUCT_HOOKS_PROPERTY].push(hookName);
+  }
+
+  private async injectDependencies(bean: Bean) {
+    const beanInstance = bean.instance;
+    const injections = beanInstance[INJECTIONS_PROPERTY];
+    if (injections !== undefined) {
+      for (const { propertyKey, propertyBeanName } of injections) {
+        const propertyBeanDescriptor = this.beanDescriptors[propertyBeanName];
+        if (propertyBeanDescriptor === undefined) {
+          throw new VernalError(`Bean descriptor of class '${propertyBeanName}' could not be found`);
+        }
+        let propertyBean;
+        if (propertyBeanDescriptor.beanType === BeanType.SINGLETON || propertyBeanDescriptor.beanType === BeanType.VALUE) {
+          propertyBean = this.beans[propertyBeanName];
+          if (propertyBean === undefined) {
+            throw new VernalError(`Bean of class '${propertyBeanName}' could not be found`);
+          }
+        } else if (propertyBeanDescriptor.beanType === BeanType.PROTOTYPE) {
+          // Construct bean from prototype
+          propertyBean = <Bean>{ instance: new propertyBeanDescriptor.beanClass() };
+          // Inject dependencies & run post construct hooks
+          await this.injectDependencies(propertyBean);
+          await this.runPostConstructHooks(propertyBean);
+        } else {
+          throw new VernalError(`Unknown bean type '${propertyBeanDescriptor.beanType}'`);
+        }
+        Object.defineProperty(beanInstance, propertyKey, {
+          configurable: false,
+          writable: false,
+          value: propertyBean,
+        });
+      }
+    }
+  }
+  private async runPostConstructHooks(bean: Bean) {
+    const beanInstance = bean.instance;
+    const postConstructHooks = beanInstance[POST_CONSTRUCT_HOOKS_PROPERTY];
+    if (postConstructHooks !== undefined) {
+      for (const hookName of postConstructHooks) {
+        const postConstructHook = beanInstance[hookName].bind(beanInstance);
+        await postConstructHook();
+      }
+    }
   }
 
   async run() {
     try {
-      // Inject dependencies
-      for (const {
-        targetName,
-        propertyKey,
-        componentName,
-      } of this.autowires) {
-        if (!this.hasComponent(targetName)) {
-          throw new VernalInjectComponentError(`Could not find component '${targetName}' in context while trying to inject into it`);
-        }
-        if (!this.hasComponent(componentName)) {
-          throw new VernalInjectComponentError(`Could not find component '${componentName}' in context while trying to inject into '${targetName}.${propertyKey}'`);
-        }
-        const target = await this.getComponent(targetName);
-        const component = await this.getComponent(componentName);
-        Object.defineProperty(target, propertyKey, {
-          value: component,
-          writable: false,
-          configurable: false,
-        });
-      }
-      // Run init (if defined)
-      for (const componentName of Object.keys(this.components)) {
-        const component = this.components[componentName];
-        if (component.type === ComponentType.SINGLETON) {
-          await this.callInitIfAvailable(component.data);
+      // Construct all the singletons
+      for (const beanName of Object.keys(this.beanDescriptors)) {
+        const { beanClass, beanType } = this.beanDescriptors[beanName];
+        if (beanType === BeanType.SINGLETON) {
+          if ({}.hasOwnProperty.call(this.beans, beanName)) {
+            throw new VernalError(`Singleton bean of class '${beanName}' already exists`);
+          }
+          this.beans[beanName] = <Bean>{ instance: new beanClass() };
         }
       }
-      // Run plugins
-      for (const plugin of this.plugins) {
-        try {
-          await plugin.run();
-        } catch (e) {
-          throw new VernalPluginError(`Plugin error in '${plugin.constructor.name}'`, e);
-        }
+      // Inject dependencies & run post construct hooks
+      for (const beanName of Object.keys(this.beans)) {
+        const bean = this.beans[beanName];
+        await this.injectDependencies(bean);
+        await this.runPostConstructHooks(bean);
       }
     } catch (e) {
-      throw new VernalRunError('Boot error', e);
+      throw new VernalError('Boot error', e);
     }
   }
 }
 
 export const Vernal = new VernalApplication();
 
-export function Component(type = ComponentType.SINGLETON) {
-  return (component: any) => {
-    return Vernal.registerComponent(component, type);
+export function Component(beanType = BeanType.SINGLETON) {
+  return (beanClass: { new (): any }) => {
+    return Vernal.registerBean(beanClass, beanType);
   }
 }
 
-export function Autowire(component: string);
-export function Autowire(component: any);
+export function Singleton(beanClass: { new (): any }) {
+  return Component(BeanType.SINGLETON)(beanClass);
+}
 
-export function Autowire(component: any) {
-  return Vernal.linkComponent(component);
+export function Prototype(beanClass: { new (): any }) {
+  return Component(BeanType.PROTOTYPE)(beanClass);
+}
+
+export function Inject(beanName: string);
+export function Inject(beanClass: { new (): any });
+export function Inject(propertyBeanNameOrClass: string | { new (): any }) {
+  return (beanInstance: Object, propertyKey: String, descriptor: PropertyDescriptor) => {
+    return Vernal.registerInjection(propertyBeanNameOrClass, beanInstance, propertyKey, descriptor);
+  }
+}
+
+export const Autowire = Inject;
+
+export function PostConstruct(beanInstance: Object, hookName: String) {
+  return Vernal.registerPostConstructHook(beanInstance, hookName);
 }
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,23 +1,73 @@
-import { Vernal, Component, Autowire } from "..";
-import { Expose } from "./Expose";
+import { Vernal, Singleton, Prototype, Inject, PostConstruct } from "..";
 
-@Component()
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:');
+  console.error(err);
+});
+
+class X {
+}
+
+Vernal.registerInstance(new X());
+
+Vernal.registerValue('V', { text: 'abc' });
+
+@Prototype
 class A {
+  public n = Math.random();
+  @Inject('D')
+  private d: any;
+  @Inject(X)
+  private x: any;
+  @Inject('V')
+  private v: any;
+
+  @PostConstruct
   async init() {
     console.log('A.init');
+    console.log(this.d);
+    console.log(this.x);
+    console.log(this.v);
   }
 }
 
-@Expose()
-@Component()
+@Singleton
 class B {
-    @Autowire(A)
-    private a: any;
+  @Inject(A)
+  private a: any;
 
-    async init() {
-      console.log('B.init');
-      console.log(this.a);
-    }
+  @PostConstruct
+  async init() {
+    console.log('B.init');
+    console.log(this.a);
+  }
+}
+
+@Singleton
+class C {
+  @Inject(A)
+  private a: any;
+
+  @PostConstruct
+  async init() {
+    console.log('C.init');
+    console.log(this.a);
+  }
+}
+
+@Singleton
+class D {
+  @Inject(B)
+  private b: any;
+  @Inject(C)
+  private c: any;
+
+  @PostConstruct
+  async init() {
+    console.log('D.init');
+    console.log(this.b);
+    console.log(this.c);
+  }
 }
 
 (async () => {
